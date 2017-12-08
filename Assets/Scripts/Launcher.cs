d525a2bc3cde81d821ab495ec73ab5f06b6d41b7
Xaunaught <riley5694@gmail.com>
2017-12-08 16:54:12 +1030

    Start of code. FIsh can stick to man

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour {
+    private Rigidbody rb;
+    public float Thrust;
+    public GameObject HumanHit;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space"))
+        {
+            rb.useGravity = true;
+            rb.AddForce(transform.forward * Thrust, ForceMode.Impulse);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Human")
+        {
+            HumanHit = other.gameObject;
+            Debug.Log("hit the player");
+            transform.SetParent(HumanHit.transform);
+            rb.isKinematic = true;
+        }
+    }
+}
