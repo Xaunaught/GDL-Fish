42ecc12f4aa2d57b808b6e19861f6346f08dbc22
Riley Huppatz <riley5694@gmail.com>
2017-12-09 22:03:20 +1030

    Fish can be thrown and instantiate

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class Launcher : MonoBehaviour {
     private Rigidbody rb;
-    public float Thrust;
-    public GameObject HumanHit;
+    
+	public GameObject BobblePrefab;
+
+	public float Charge;
+	public float Thrust;
+	public float ChargeMultipliyer = 10;
+	public float MaxForce = 40;
+	public bool increasing = true;
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown("space"))
-        {
+		if (Input.GetButton("Jump"))
+		{
+			if (increasing)
+			{
+				Charge += Time.deltaTime * ChargeMultipliyer;
+			}
+			if (!increasing)
+			{
+				Charge -= Time.deltaTime * ChargeMultipliyer;
+			}
+			if(Charge >= MaxForce)
+			{
+				increasing = false;
+			}
+			if (Charge < 0)
+			{
+				increasing = true;
+			}
+			
+		}
+		
+		if(Input.GetKeyUp("space"))
+		{
+			GameObject clone = Instantiate(BobblePrefab);
+			rb = clone.GetComponent<Rigidbody>();
             rb.useGravity = true;
-            rb.AddForce(transform.forward * Thrust, ForceMode.Impulse);
-        }
+			rb.AddForce(transform.forward * (Charge * Thrust), ForceMode.Impulse);
+			Charge = 0;
+		}
 	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Human")
-        {
-            HumanHit = other.gameObject;
-            Debug.Log("hit the player");
-            transform.SetParent(HumanHit.transform);
-            rb.isKinematic = true;
-        }
-    }
+    
 }
---
+++ Assets/Scripts/StickToHuman.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickToHuman : MonoBehaviour {
+
+	public GameObject HumanHit;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Human")
+		{
+			HumanHit = other.gameObject;
+			Debug.Log("hit the player");
+			transform.SetParent(HumanHit.transform);
+			rb.isKinematic = true;
+		}
+	}
+}
